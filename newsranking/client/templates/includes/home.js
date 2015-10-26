c3138f768c04912e3bc21c6215a9eca6794817de
Abzal Serekov <abzal.serekov@gmail.com>
2015-10-26 11:35:21 +0600

    templates are generated for meteor

--- newsranking/client/templates/includes/home.js
+++ newsranking/client/templates/includes/home.js
@@ -17,5 +17,15 @@ Template.home.events({
 			link: l,
 			added: new Date(),
 		});
+	},
+	'click .upvote': function (e) {
+		e.preventDefault();
+		var _id = $(e.currentTarget).attr('id');
+		Links.update({_id: _id}, {$inc: {votes: 1}});
+	},
+	'click .article-meta-box__name': function (e) {
+		var _id = $(e.currentTarget).attr('id').substring(5); // "title" is removed out of the id
+		Links.update({_id: _id}, {$inc: {views: 1}});		
 	}
+
 });
