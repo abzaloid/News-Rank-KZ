2d804fe79794dc42c8ac69ecdaff7e83aba77f3c
Abzal Serekov <abzal.serekov@gmail.com>
2015-10-26 11:34:41 +0600

    database model for LINK table is updated

--- newsranking/server/fixtures.js
+++ newsranking/server/fixtures.js
@@ -1,7 +1,9 @@
 if (Links.find().count() === 0) {
 	Links.insert({
 		votes: 1,
+		title: "Срочно в номер! Пока яблок сезон .. печем булки!",
 		link: "http://yvision.kz/post/578092",
 		added: new Date(),
+		views: 1,
 	});
 }
