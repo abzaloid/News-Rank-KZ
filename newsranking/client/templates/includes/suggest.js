0555afde5d4663c1d564ccdd78738742ac303928
Abzal Serekov <abzal.serekov@gmail.com>
2015-10-26 13:43:31 +0600

    checking submitted url for correctness

--- newsranking/client/templates/includes/suggest.js
+++ newsranking/client/templates/includes/suggest.js
@@ -13,22 +13,38 @@ Template.suggest.events({
 		var url = $("#url-send").val(),
 			title = $("#title-send").val();
 
-		if (!url || !title) {
+		if (!url) {
 			Notifications.error('Заполните все поля!', 'Нужно ввести ссылку на статью и ее название');		    
 			return;
 		}
 
-		Links.insert({
-			link: url,
-			title: title,
-			votes: 1,
-			views: 0, 
-			added: new Date(),
-		});
+		if (url.length > 7) {
+			if (url.substring(0, 7) !== "http://")
+				url = "http://" + url; 
+		} else {
+			url = "http://" + url;
+		}
 
-		Notifications.success('Спасибо!', 'Новая ссылка добавлена');		    
+		$("#url-send").val(url);
+		
+		$("#sacha-spinner").show();
 
-		Router.go('home');
-
+		HTTP.call("GET", url, {},
+          function (error, result) {
+			$("#sacha-spinner").hide();        
+            if (error) {
+            	Notifications.error('Неправильный адрес!', 'Пожалуйста, проверьте правильность введенного вами адреса');
+            } else {
+				Links.insert({
+					link: url,
+					title: title,
+					votes: 1,
+					views: 0, 
+					added: new Date(),
+				});
+				Notifications.success('Спасибо!', 'Новая ссылка добавлена');		    
+				Router.go('home');
+            }
+          });
 	}
 });
---
+++ newsranking/lib/config.js
@@ -0,0 +1,5 @@
+// // Listen to incoming HTTP requests, can only be used on the server
+// WebApp.connectHandlers.use(function(req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   return next();
+// });
